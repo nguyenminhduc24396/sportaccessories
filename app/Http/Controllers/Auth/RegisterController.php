7e28033efcab6dbf67b7c9ee95ffe6166a854a1a
nguyenminhduc <nguyenminhduc24396@gmail.com>
2018-12-10 11:30:02 +0700

    121212121

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/email/verify';
+    protected $redirectTo = '/login';
 
     /**
      * Create a new controller instance.
